61a5fd53c6877546aab30225bc469e00a24d5434
David Zhu <davidyzhu2@gmail.com>
2024-11-12 17:04:26 -0800

    Add functions for human and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let choice = Math.random() * 3;
+    if (choice < 1) {
+        return "rock";
+    } else if (choice < 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Let's play rock paper scissors! Enter your choice:");
+    return choice;
+}
+
+console.log(getHumanChoice());
