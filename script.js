d3ce313d747d969ab40c16afb2fc457ef2fed760
David Zhu <davidyzhu2@gmail.com>
2024-11-12 18:55:07 -0800

    Write basic logic for one round of rock paper scissors

--- script.js
+++ script.js
@@ -14,4 +14,43 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "rock") {
+                console.log("Tie! Both chose rock!");
+            } else if (computerChoice === "paper") {
+                console.log("You lose! Paper beats rock!");
+            } else {
+                console.log("You win! Rock beats scissors!");
+            }
+            break;
+        case "paper":
+            if (computerChoice === "rock") {
+                console.log("You win! Paper beats rock!");
+            } else if (computerChoice === "paper") {
+                console.log("Tie! Both chose paper!");
+            } else {
+                console.log("You lose! Scissors beats paper!");
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats scissors!");
+            } else if (computerChoice === "paper") {
+                console.log("You win! Scissors beats paper!");
+            } else {
+                console.log("Tie! Both chose scissors!");
+            }
+            break;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
