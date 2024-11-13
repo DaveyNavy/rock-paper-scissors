c0c1060d01b874a465704823fbff891e07058d5b
David Zhu <davidyzhu2@gmail.com>
2024-11-12 19:00:33 -0800

    Add ability to play five rounds of rock paper scissors, plus score tracking

--- script.js
+++ script.js
@@ -14,43 +14,67 @@ function getHumanChoice() {
     return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    switch (humanChoice) {
-        case "rock":
-            if (computerChoice === "rock") {
-                console.log("Tie! Both chose rock!");
-            } else if (computerChoice === "paper") {
-                console.log("You lose! Paper beats rock!");
-            } else {
-                console.log("You win! Rock beats scissors!");
-            }
-            break;
-        case "paper":
-            if (computerChoice === "rock") {
-                console.log("You win! Paper beats rock!");
-            } else if (computerChoice === "paper") {
-                console.log("Tie! Both chose paper!");
-            } else {
-                console.log("You lose! Scissors beats paper!");
-            }
-            break;
-        case "scissors":
-            if (computerChoice === "rock") {
-                console.log("You lose! Rock beats scissors!");
-            } else if (computerChoice === "paper") {
-                console.log("You win! Scissors beats paper!");
-            } else {
-                console.log("Tie! Both chose scissors!");
-            }
-            break;
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "rock") {
+                    console.log("Tie! Both chose rock!");
+                    return "Tie";
+                } else if (computerChoice === "paper") {
+                    console.log("You lose! Paper beats rock!");
+                    return "Loss";
+                } else {
+                    console.log("You win! Rock beats scissors!");
+                    return "Win";
+                }
+                break;
+            case "paper":
+                if (computerChoice === "rock") {
+                    console.log("You win! Paper beats rock!");
+                    return "Win";
+                } else if (computerChoice === "paper") {
+                    console.log("Tie! Both chose paper!");
+                    return "Tie";
+                } else {
+                    console.log("You lose! Scissors beats paper!");
+                    return "Loss";
+                }
+                break;
+            case "scissors":
+                if (computerChoice === "rock") {
+                    console.log("You lose! Rock beats scissors!");
+                    return "Loss";
+                } else if (computerChoice === "paper") {
+                    console.log("You win! Scissors beats paper!");
+                    return "Win";
+                } else {
+                    console.log("Tie! Both chose scissors!");
+                    return "Tie";
+                }
+                break;
+        }
     }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        
+        let result = playRound(humanSelection, computerSelection);
+        if (result === "Win") {
+            humanScore++;
+        } else if (result == "Loss") {
+            computerScore++;
+        }
+    }
+
+    console.log("Game finished! Your score was " + humanScore + ". The computer's score was " + computerScore);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
